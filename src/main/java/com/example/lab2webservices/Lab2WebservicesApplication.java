0b302cb20ac5934a745d1643949d021dc66d371f
Eva Hottinen <ehottinen@gmail.com>
2025-05-13 12:01:38 +0200

    Part of the Initial commit I missed

---
+++ src/main/java/com/example/lab2webservices/Lab2WebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab2webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2WebservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2WebservicesApplication.class, args);
+    }
+
+}
