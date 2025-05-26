cfb65738373ce5acd9c3d618d59771e367dfd68d
Eva Hottinen <ehottinen@gmail.com>
2025-05-26 09:24:07 +0200

    clean-up

--- jokeservice/src/main/java/com/example/jokeservice/config/SecurityConfig.java
+++ jokeservice/src/main/java/com/example/jokeservice/config/SecurityConfig.java
@@ -2,18 +2,8 @@ package com.example.jokeservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
-import java.security.Key;
-
-import static java.security.KeyRep.Type.SECRET;
 
 @Configuration
 public class SecurityConfig {
